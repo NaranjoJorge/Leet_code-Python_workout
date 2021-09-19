9afd0ff7bf2d319676c52eac351e365ed1b202a6
Jorge Naranjo <jpna188@gmail.com>
2021-09-19 15:34:16 -0500

    Changed class for function

--- easy/1_two_sum.py
+++ easy/1_two_sum.py
@@ -1,13 +1,12 @@
 #Given an array of integers nums and an integer target, 
 #return indices of the two numbers such that they add up to target.
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        while i < len(nums) - 1:
-	        j = i + 1
-	        while j < len(nums):
-		        if nums[i] + nums[j] == target:
-			        return[i, j]
-		        j += 1
-	        i += 1
+def twoSum(nums, target):
+    i = 0
+    while i < len(nums) - 1:
+	    j = i + 1
+	    while j < len(nums):
+		    if nums[i] + nums[j] == target:
+			    return[i, j]
+		    j += 1
+	    i += 1
