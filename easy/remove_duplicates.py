d3692c4c7e2e6ce58519bcda79a8b5936390e831
Jorge Naranjo <jpna188@gmail.com>
2021-09-19 18:20:09 -0500

    Function that removes duplicates from list and returns int k of elements in new non-repetitive list

---
+++ easy/remove_duplicates.py
@@ -0,0 +1,20 @@
+#Given an integer array nums sorted in non-decreasing order, 
+#remove the duplicates in-place such that each unique element appears only once. 
+#The relative order of the elements should be kept the same.
+
+#Input: nums = [0,0,1,1,1,2,2,3,3,4]
+#Output: 5, nums = [0,1,2,3,4]
+
+def removeDuplicates(nums):
+	k = i = 0
+	while i < len(nums) - 1:
+		j = i + 1
+		while j < len(nums):
+			if nums[i] == nums[j]:
+				nums.pop(j)
+				continue
+			j += 1
+		i += 1
+	for x in nums:
+		k += 1
+	return(k)
