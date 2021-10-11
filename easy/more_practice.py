04c099c9885fd29a47bb964fb82d2f02520c4626
Jorge Naranjo <jpna188@gmail.com>
2021-10-11 10:26:44 -0500

    float number function

--- easy/more_practice.py
+++ easy/more_practice.py
@@ -1,2 +1,22 @@
-guess = float(input('here: '))
-print(guess)
+'''
+@f_number: Takes a float(z) and two integers (before and after). 
+The function should return a float consisting of before digits 
+before the decimal point and after digits after.
+
+@z : float
+@before: int
+@after: int
+'''
+
+def float_number(z, before, after):
+	count = 1
+	before = str(int(z))[-before:]
+	for i in range(after):
+		count *= 10
+	z *= count
+	after = str(int(z))[-after:]
+	z = float(before + '.' + after)
+	return(z)
+
+#print(float_number(12345.9987,4,2))
+
