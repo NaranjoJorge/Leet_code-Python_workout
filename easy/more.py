e7c730c8feeca4bfb2234f338264d2882f892168
Jorge Naranjo <jpna188@gmail.com>
2021-10-13 16:38:23 -0500

    Translate to ubbi considering capitalized letters.

--- easy/more.py
+++ easy/more.py
@@ -1,3 +1,17 @@
-cosa = 'casa'
-print(cosa[1:2])
-	
+'''
+@capitalize_ubbi(word): Translates word to Ubbi language 
+considering capitalized words.
+
+@word: String
+'''
+def capitalized_ubbi(word):
+	new = []
+	for i in word:
+		if i in 'aeiouAEIOU':
+			new.append(f'ub{i}')
+		else:
+			new.append(i)
+	return ''.join(new)
+
+#print(capitalized_ubbi('Reed'))
+
