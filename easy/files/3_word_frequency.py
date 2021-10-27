a8a2feb303925b983df511e11fc95739056f679b
Jorge Naranjo <jpna188@gmail.com>
2021-10-27 10:22:24 -0500

    Words frequency dict

---
+++ easy/files/3_word_frequency.py
@@ -0,0 +1,17 @@
+'''
+@words_frequency(): Returns frequency of words in file as a dict.
+'''
+from collections import defaultdict
+from json import dumps
+
+def words_frequency():
+	d = defaultdict(int)
+	file = input('Enter file name: ')
+	words = input('Enter words: ')
+
+	for word in words.split():	
+		d[word] += "".join(open(file)).split().count(word)
+	
+	return dumps(d, indent=4, sort_keys=True)
+
+print(words_frequency())
